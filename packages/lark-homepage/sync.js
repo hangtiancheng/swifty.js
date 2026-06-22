38d70be8fe345e0d582d98e1f363a18d9624eb3f
hangtiancheng.htc <hangtiancheng.htc@taobao.com>
2026-06-22 19:11:43 +0800

    ci: Deploy homepage

--- packages/lark-homepage/sync.js
+++ packages/lark-homepage/sync.js
@@ -8,7 +8,7 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const REPO = "git@github.com:hangtiancheng/h.git";
+const REPO = "https://github.com/hangtiancheng/h.git";
 const TEMP_DIR = resolve(__dirname, "h");
 const DOCS_DIR = resolve(__dirname, "docs");
 if (existsSync(DOCS_DIR)) {
