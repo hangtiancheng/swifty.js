d9446b286419837d8eeffde793216b5788110018
hangtiancheng.htc <hangtiancheng.htc@taobao.com>
2026-07-17 13:09:31 +0800

    feat: Introduce new features

--- packages/hooks/src/index.ts
+++ packages/hooks/src/index.ts
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2026 hangtiancheng
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 export { default as useCreation } from "./use-creation/index.js";
 export { default as useLatest } from "./use-latest/index.js";
 export { default as useMemoizedFn } from "./use-memorized-fn/index.js";
